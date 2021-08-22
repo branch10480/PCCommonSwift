e6c1e73af3eb13552760fc679209b1aa06335e4d
branch10480 <toshiharu.imaeda@gmail.com>
2021-08-22 17:02:43 +0900

    2021/08/22 1702

--- Sources/PCCommon/PCCommon.swift
+++ Sources/PCCommon/PCCommon.swift
@@ -1,16 +1,24 @@
 public func readLineString() -> String {
-  readLine()!
+  printBeforeInput()
+  return readLine()!
 }
 
 public func readLineInt() -> Int {
-  Int(readLineString())!
+  printBeforeInput()
+  return Int(readLineString())!
 }
 
 public func readLineAndGetInts(separetedBy separator: Character) -> [Int] {
-  readLineString().split(separator: separator).map { Int(String($0))! }
+  printBeforeInput()
+  return readLineString().split(separator: separator).map { Int(String($0))! }
 }
 
 public func readLineAndGetTwoInts(separetedBy separator: Character) -> (Int, Int) {
+  printBeforeInput()
   let ints = readLineAndGetInts(separetedBy: separator)
   return (ints[0], ints[1])
+}
+
+fileprivate func printBeforeInput() {
+  print("Please input", terminator: ": ")
 }
