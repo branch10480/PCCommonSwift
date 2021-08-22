6dfc949db5c0b58d410c30b6f20dbea9553030d5
branch10480 <toshiharu.imaeda@gmail.com>
2021-08-22 16:53:50 +0900

    2021/08/22 1653

--- Sources/PCCommon/PCCommon.swift
+++ Sources/PCCommon/PCCommon.swift
@@ -1,3 +1,16 @@
-struct PCCommon {
-    var text = "Hello, World!"
+public func readLineString() -> String {
+  readLine()!
+}
+
+public func readLineInt() -> Int {
+  Int(readLineString())!
+}
+
+public func readLineAndGetInts(separetedBy separator: Character) -> [Int] {
+  readLineString().split(separator: separator).map { Int(String($0))! }
+}
+
+public func readLineAndGetTwoInts(separetedBy separator: Character) -> (Int, Int) {
+  let ints = readLineAndGetInts(separetedBy: separator)
+  return (ints[0], ints[1])
 }
