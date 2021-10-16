1b53e59877223f4709074fcc3bc853c3e8c411fa
branch10480 <toshiharu.imaeda@gmail.com>
2021-10-16 14:45:12 +0900

    Update

--- Sources/PCCommon/PCCommon.swift
+++ Sources/PCCommon/PCCommon.swift
@@ -1,24 +1,25 @@
-public func readLineString() -> String {
-  printBeforeInput()
+public func readLineString(message: String? = nil) -> String {
+  printBeforeInput(message: message)
   return readLine()!
 }
 
-public func readLineInt() -> Int {
-  printBeforeInput()
+public func readLineInt(message: String? = nil) -> Int {
+  printBeforeInput(message: message)
   return Int(readLineString())!
 }
 
-public func readLineAndGetInts(separetedBy separator: Character) -> [Int] {
-  printBeforeInput()
+public func readLineAndGetInts(separetedBy separator: Character, message: String? = nil) -> [Int] {
   return readLineString().split(separator: separator).map { Int(String($0))! }
 }
 
-public func readLineAndGetTwoInts(separetedBy separator: Character) -> (Int, Int) {
-  printBeforeInput()
+public func readLineAndGetTwoInts(separetedBy separator: Character, message: String? = nil) -> (Int, Int) {
   let ints = readLineAndGetInts(separetedBy: separator)
   return (ints[0], ints[1])
 }
 
-fileprivate func printBeforeInput() {
+fileprivate func printBeforeInput(message: String? = nil) {
+  if let message = message {
+    print(message)
+  }
   print("Please input", terminator: ": ")
 }
